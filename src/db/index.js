4b09e54840f9151f20c1dbeb5516790db222ce2b
Samarth-Ad <samarthad950@gmail.com>
2025-12-06 13:40:36 +0530

    DB connected successfully :)

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+import { DB_NAME } from "../constants.js";
 
 const DB_URI = process.env.MONGODB_URI ;
 
 const connectDB = async function (){
     try {
         const connectionInstance = await mongoose.connect(`${DB_URI}/${DB_NAME}`);
+        //try to read this API
+        // console.log(connectionInstance)
         console.log(`MongoDB Connected \n DB Host : ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("MonogoDB connection error : ",error);
