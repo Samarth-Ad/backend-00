e710fb541e9cc68050232b3bfd67cf41d8159254
Samarth-Ad <samarthad950@gmail.com>
2025-12-06 13:27:27 +0530

    try to connect DB

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+
+const DB_URI = process.env.MONGODB_URI ;
+
+const connectDB = async function (){
+    try {
+        const connectionInstance = await mongoose.connect(`${DB_URI}/${DB_NAME}`);
+        console.log(`MongoDB Connected \n DB Host : ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("MonogoDB connection error : ",error);
+        process.exit(1);
+    }
+};
+
+export default connectDB ;
---
+++ src/index.js
@@ -0,0 +1,45 @@
+// require('dotenv').config({path : "./env"})
+import dotenv from "dotenv" ;
+dotenv.config({path : "./env"})
+import connectDB from "./db/index.js";
+
+
+const PORT = process.env.PORT;
+
+connectDB();
+
+
+
+
+/*
+// This approach is fine but not professional at all
+// const connectDB = function (){
+    
+// }
+
+// connectDB();
+
+//Instead we use IIFE -> Immediately Invoked Function Expression
+// Immediately executed (invoked) on the spot.
+
+
+import express from "express";
+const app = express();
+(async ()=>{
+    try {
+        await mongoose.connect(`${DB_URI}/${DB_NAME}`);
+        // In-case the express app couldn't connect to DB 
+        app.on("error",(error)=>{
+            console.log("Error : ",error);
+            throw error;
+        })
+
+        app.listen(PORT,()=>{
+            console.log(`App is listening on PORT : ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+})()
+*/
