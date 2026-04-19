112a3ed0c1f956d45cbf97451703e93196402373
Samarth-Ad <samarthad950@gmail.com>
2026-04-19 23:56:08 +0530

    For railway deployment

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async function () {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI)
         //try to read this API
         // console.log(connectionInstance)
         console.log(`MongoDB Connected \n DB Host : ${connectionInstance.connection.host}`)
